4ecfb2e975a4df915f9038c41fde7b9b48d7b60b
CHISOMO1234 <cchitsanya@gmail.com>
2023-08-11 00:36:39 -0700

    Task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a program to print a line with put function
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+puts("\"Programming is like builing a multilingual puzzle");
+return (0);
+}
